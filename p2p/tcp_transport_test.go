ca5bfc284765a074d051d7ec7e251d8027361a9d
Guiziin227 <gh63388@gmail.com>
2026-09-21 20:47:14 -0300

    add TCP transport implementation and corresponding tests

---
+++ p2p/tcp_transport_test.go
@@ -0,0 +1,17 @@
+package p2p
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTCPTransport(t *testing.T) {
+	listenAddress := ":8080"
+
+	tr := NewTCPTransport(listenAddress)
+
+	assert.Equal(t, listenAddress, tr.listenAddress)
+
+	assert.Nil(t, tr.ListenAndAccept())
+}
